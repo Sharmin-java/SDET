9fad9775b5a4ff1d53e24687b05f232b874f093c
Sharmin-java <60256055+Sharmin-java@users.noreply.github.com>
2020-01-25 12:46:59 +0530

    Update LongestWord.java

--- LongestWord.java
+++ LongestWord.java
@@ -5,19 +5,21 @@ public class LongestWord {
 	 * @param - 
 	 * 		paragraph 
 	 * Assumptions -
-	 * 		1.There is distinct longest word in the parameter
-	 * 		2.There is no punctuation mark in the parameter
+	 * 		1.There is distinct longest word in the sentence
+	 * 		2.The only word separator should be space character in the sentence 
 	 */
-	
 	public String getLongestWord(String paragraph) {
-		
-        	String [] words = paragraph.split(" ");
-        	String longestWord = "";
-       	 	for(int i = 0; i < words.length; i++){
-        		if(words[i].length() >= longestWord.length()){
-        			longestWord = words[i];
-        		} 
-        	}
-		return longestWord.length() + " and " + longestWord;
+			//replacing all the punctuation marks
+			String sentence = paragraph.replaceAll("[^a-zA-Z ]", "");
+			//split the sentence into words 
+	        String [] words = sentence.split(" ");
+	        String longestWord = "";
+	        for(int i = 0; i < words.length; i++){
+	        	if(words[i].length() >= longestWord.length()){
+	        		longestWord = words[i];
+	        	} 
+	        }
+			return longestWord.isEmpty()?longestWord:longestWord.length() + " and " + longestWord;
 	}
+	
 }//end class 
